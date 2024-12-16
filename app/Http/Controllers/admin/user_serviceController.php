ab67a1577193961041e4f23455af6ea5cc3faf49
Yves Fouda <118168502+Like-Lejeune@users.noreply.github.com>
2024-12-16 16:35:03 +0100

    Update user_serviceController.php

--- app/Http/Controllers/admin/user_serviceController.php
+++ app/Http/Controllers/admin/user_serviceController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 use App\Http\Controllers\Tools;
 use Illuminate\Database\QueryException;
+use App\Models\Service;
 
 class user_serviceController extends Controller
 {
@@ -21,6 +22,38 @@ class user_serviceController extends Controller
         $this->tools = $tools;
     }
 
+    public function userlist()
+    {
+        $nbuser = User::count();
+        $nbuserUnactive = User::where('status', '!==', 1)->count();
+        return view('admin.listUser')->with('nbuser',$nbuser)->with('nbuserUnactive',$nbuserUnactive);
+    }
+
+    public function updateUser_(string $id)
+    {
+        $user = User::where('id', $id)->first();
+        $user_service = DB::table('services')
+        ->join('user_service', 'id_service', '=', 'service_id')
+        ->where('user_id', '=',$id)
+        ->get();
+        return view('admin.updateUser')->with('user',$user)->with('user_service',$user_service);
+    }
+
+    public function formUser()
+    {
+        //
+
+        try {
+            //$service = DB::table('services')
+            $service = service::orderby('s_name', 'ASC')->get();
+            return view('admin.formulaireUsers')->with('services',$service);
+
+        } catch (QueryException $e) {
+
+            return response()->json($e->getMessage());
+        }
+    }
+    
     // Create User -- Type User //
 
     public function nouvelUser(Request $request)
@@ -37,9 +70,9 @@ class user_serviceController extends Controller
 
         } else {
                 if ($request->input('password') !== null && $request->input('password') !== '') {
-                    $password = $this->tools->crypterChaine($this->tools->controle_space($request->input('password')));
+                    $password = $this->tools->krypt($this->tools->controle_space($request->input('password')));
                 } else {
-                    $password = $this->tools->crypterChaine('Compta2024');
+                    $password = $this->tools->krypt('Compta2024');
                 }
                 if ($request->input('type') !== null && $request->input('type') !== '') {
                     $type = $this->tools->$request->input('type');
@@ -94,13 +127,10 @@ class user_serviceController extends Controller
                     ->update(array(
                         'name' => $request->input('name') !== null ? $this->tools->controle_space($request->input('name')) : $user->name,
                         'email' => $request->input('email') !== null ? $this->tools->controle_space($request->input('email')) : $user->email,
-                        'password' => $request->input('password') !== null ? $this->tools->crypterChaine($this->tools->controle_space($request->input('password'))) : $user->password,
+                        'password' => $this->tools->krypt($request->input('password')) !== null ? $this->tools->krypt($this->tools->controle_space($request->input('password'))) : $user->password,
                         'type' => $request->input('type') !== null ? $this->tools->controle_space($request->input('type')) : $user->type,
                         'updated_at' => $this->tools->HeureLocale(),
                     ));
-
-                
-                    return redirect()->back()->with('success', 'You have successfully update user.');
             }
 
             if ($request->has('service')) {
@@ -133,8 +163,9 @@ class user_serviceController extends Controller
                         ->delete();
                 }
             }
+            
+            return redirect()->back()->with('success', 'You have successfully update user.');
 
-            return response()->json(['message' => 'Update Ok'], 200);
         } catch (QueryException $e) {
 
             return response()->json($e->getMessage());
@@ -148,7 +179,7 @@ class user_serviceController extends Controller
             $user = DB::table('users')
                 ->where('id', $id)
                 ->first();
-            if ($user) {
+            if ($user) { 
                 $user = DB::table('users')
                     ->where('id', $id)
                     ->delete();
@@ -192,4 +223,37 @@ class user_serviceController extends Controller
             return response()->json(['error' => 'Une erreur s\'est produite : ' . $e->getMessage()], 500);
         }
     }
+
+    public function show(string $id)
+    {
+        //
+
+        try {
+
+            $user = DB::table('users')
+                ->where('id', $id)
+                ->select("*")
+                ->first();
+            $roles = DB::table('services')
+                    ->join('user_service', 'service_id', '=', 'id_service')
+                    ->where('user_id', $user->id)
+                    ->pluck('services');
+            $idRole= DB::table('roles')
+                    ->join('user_role', 'idRole', '=', 'role_id')
+                    ->where('user_id', $user->id)
+                    ->pluck('idRole');
+
+                    $user->roles = $roles;
+                    $user->role_id = $idRole;
+           
+            if ($user) {
+                return response()->json($user);
+            }
+            return response()->json(['message' => 'User not found'], 404);
+        } catch (QueryException $e) {
+
+            return response()->json($e->getMessage());
+        }
+    }
+
 }
